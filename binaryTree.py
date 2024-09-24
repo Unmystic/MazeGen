1cee5a822acf30d3d59893e216cf5c7e2ddd6456
Unmystic <unmystic7@gmail.com>
2024-09-24 12:17:41 +0200

    Added binary tree to mg

---
+++ binaryTree.py
@@ -0,0 +1,24 @@
+from grid import Cell, Grid
+import random
+
+
+class BinaryTree(object):
+    
+    def on(self, grid):
+        for cell in grid.each_cell():
+            neighbors = []
+            if cell.north:
+                neighbors.append(cell.north)
+            if cell.east:
+                neighbors.append(cell.east)
+            
+            if neighbors:            
+                neighbor = random.choice(neighbors)
+                cell.link(neighbor)
+        return grid
+    
+
+if __name__ == "__main__":
+    grid = Grid(4,4)
+    tree = BinaryTree()
+    tree.on(grid)
