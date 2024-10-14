5b320cc39e812e988a304c34d5824c01ec087600
Unmystic <unmystic7@gmail.com>
2024-10-14 10:00:01 +0200

    Add wilsons algo + fuction to join colored mazes into single image

--- ald_bro.py
+++ ald_bro.py
@@ -3,6 +3,7 @@ import random
 from grid import Grid, ColoredGrid
 import os
 from pathlib import Path
+from wilsons import join_images
 
 
 class AldousBroder(object):
@@ -27,7 +28,7 @@ def generate_colored_mazes(n=6):
     path = Path("examples/aldous_broder")
     
     for i in range(n):
-        grid = ColoredGrid(25,25)
+        grid = ColoredGrid(20,20)
         ab = AldousBroder()
         ab_grid = ab.on(grid)
         start =ab_grid.grid[ab_grid.rows // 2][ab_grid.cols // 2]
@@ -36,9 +37,9 @@ def generate_colored_mazes(n=6):
         fname = f"colored_maze_{i+1}.png"
         new_path = os.path.join(path , Path(fname))
         
-        ab_grid.to_png(cell_size= 15, fname=new_path)
+        ab_grid.to_png(cell_size= 10, fname=new_path)
         print(f"Created image {fname} in directory {path}")
-        
+
     
     
 if __name__ == "__main__":
@@ -49,3 +50,16 @@ if __name__ == "__main__":
     
     ab_grid.to_png(cell_size=20, fname="aldous_broder.png")
     generate_colored_mazes()
+    
+    # Paths to your 6 images (adjust the paths as per your folder)
+    image_paths = [
+        "examples/aldous_broder/colored_maze_1.png", "examples/aldous_broder/colored_maze_2.png",
+        "examples/aldous_broder/colored_maze_3.png","examples/aldous_broder/colored_maze_4.png",
+        "examples/aldous_broder/colored_maze_5.png", "examples/aldous_broder/colored_maze_6.png"
+    ]
+
+    # Join the images
+    combined_image = join_images(image_paths, space_between=10)
+
+    # Save the result as a single image
+    combined_image.save("examples/aldous_broder/combined_image.png")
