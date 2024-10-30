b44af6d0c3bd03e2e175cdb984c186c52089a8ad
Unmystic <unmystic7@gmail.com>
2024-10-30 17:42:51 +0100

    added weaving maze generation

--- maze_test.py
+++ maze_test.py
@@ -1,22 +1,23 @@
 from PIL import Image, ImageDraw
 from grid import Grid, Cell
+from weave_grid import WeaveGrid
 import random
 from recursive_backtracker import RecursiveBacktracker
 
 
 if __name__ == "__main__":
-    grid = Grid(15,15)
+    grid = WeaveGrid(20,20)
     rback = RecursiveBacktracker()
     rback_grid = rback.on(grid)
     
-    deadends = rback_grid.deadends()
-    print(f"The maze has {len(deadends)} deadends")
-    rback_grid.braid(0.5)
+    # deadends = rback_grid.deadends()
+    # print(f"The maze has {len(deadends)} deadends")
+    # rback_grid.braid(0.5)
     
-    deadends = rback_grid.deadends()
-    print(f"The maze has {len(deadends)} deadends")
+    # deadends = rback_grid.deadends()
+    # print(f"The maze has {len(deadends)} deadends")
     
     #print(rback_grid)
-    
-    rback_grid.to_png(cell_size=20, fname="test_maze.png",inset=0.1)
-    print("Created image test_maze.png")
+    fname = "weave_maze.png"
+    rback_grid.to_png(cell_size=20,fname=fname, inset=0.1)
+    print(f"Created image {fname}")
--- weave_grid.py
+++ weave_grid.py
@@ -55,7 +55,9 @@ class OverCell(Cell):
         return ( not self.linked(self.east) and not self.linked(self.west) and 
                 self.linked(self.north) and self.linked(self.south))
         
+    
     def link(self, cell, bidir=True):
+        neighbor = None
         if self.north and self.north == cell.south:
             neighbor = self.north
         elif self.south and self.south == cell.north:
@@ -67,3 +69,87 @@ class OverCell(Cell):
             
         if neighbor:
             self.grid.tunnel_under(neighbor)
+        else:
+            super().link(cell, bidir)
+    
+class UnderCell(Cell):
+    def __init__(self, over_cell):
+        super().__init__(over_cell.row, over_cell.col)
+        
+        if over_cell.horizontal_passage():
+            self.north = over_cell.north
+            over_cell.north.south = self
+            self.south = over_cell.south
+            over_cell.south.north = self
+            self.link(self.north)
+            self.link(self.south)
+        else:
+            self.east = over_cell.east
+            over_cell.east.west = self
+            self.west = over_cell.west
+            over_cell.west.east = self
+            self.link(self.east)
+            self.link(self.west)
+            
+    def horizontal_passage(self):
+        if self.east or self.west:
+            return True
+        else:
+            return False
+    
+    def vertical_passage(self):
+        if self.north or self.south:
+            return True
+        else:
+            return False
+
+class WeaveGrid(Grid):
+    def __init__(self, rows, columns):
+        self.under_cells = []
+        super().__init__(rows, columns)
+        
+        
+    def prepare_grid(self):
+        grid = []
+        for i in range(self.rows):
+            row_list = []
+            for j in range(self.cols):
+                Cell(i,j)
+                row_list.append(OverCell(i,j,self))
+            grid.append(row_list)
+        return grid
+    
+    def tunnel_under(self,over_cell):
+        under_cell = UnderCell(over_cell)
+        self.under_cells.append(under_cell)
+    
+    def each_cell(self):
+        for row in self.grid:
+            for cell in row:
+                yield cell
+        
+        for cell in self.under_cells:
+            yield cell
+    
+    def to_png(self, cell_size=20, inset=0.1, fname="weave_maze.png"):
+        return super().to_png(cell_size = cell_size, inset = inset, fname=fname)
+    
+    def to_png_with_inset(self, img, d, cell, mode, cell_size, wall, x, y, inset):
+        if cell.__class__.__name__ == "OverCell":
+            
+            super().to_png_with_inset(img, d, cell, mode, cell_size, wall, x, y, inset)
+        else:
+            x1,x2,x3,x4,y1,y2,y3,y4 = self.cell_coordinates_with_inset(x,y,cell_size,inset)
+            width = 1
+            if cell.vertical_passage():
+                d.line([x2,y1,x2,y2], fill=wall,width=width)
+                d.line([x3,y1,x3,y2], fill=wall,width=width)
+                d.line([x2,y3,x2,y4], fill=wall,width=width)
+                d.line([x3,y3,x3,y4], fill=wall,width=width)
+            else:
+                d.line([x1,y2,x2,y2], fill=wall,width=width)
+                d.line([x1,y3,x2,y3], fill=wall,width=width)
+                d.line([x3,y2,x4,y2], fill=wall,width=width)
+                d.line([x3,y3,x4,y3], fill=wall,width=width)
+                
+    
